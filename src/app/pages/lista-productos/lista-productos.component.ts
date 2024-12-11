a7df8ffd7582003e3bfcab93f3ccbb1680268808
Nickygrv <nicole.rojas@ucb.edu.bo>
2024-12-11 04:04:15 -0400

    Correcciones bug

--- src/app/pages/lista-productos/lista-productos.component.ts
+++ src/app/pages/lista-productos/lista-productos.component.ts
@@ -22,6 +22,7 @@ export class ListaProductosComponent implements OnInit {
   maxPrice: number = 0;
 
   filterValue: string = '';
+  errorMessage: string = ''; // Variable para mostrar mensaje de error
 
   constructor(private http: HttpClient) {}
 
@@ -49,8 +50,23 @@ export class ListaProductosComponent implements OnInit {
   }
 
   applyPriceFilter() {
+    // Validar si el límite inferior es mayor que el superior
+    if (this.minPrice > this.maxPrice) {
+      this.errorMessage = 'El precio mínimo no puede ser mayor que el precio máximo.';
+      this.resetFilters(); // Restablecer filtros si hay un error
+      return;
+    } else if (this.maxPrice < this.minPrice) {
+      this.errorMessage = 'El precio máximo no puede ser menor que el precio mínimo.';
+      this.resetFilters(); // Restablecer filtros si hay un error
+      return;
+    }
+
+    // Borrar el mensaje de error si los precios son válidos
+    this.errorMessage = '';
+
     if (this.minPrice < 0) this.minPrice = 0;
     if (this.maxPrice < 0) this.maxPrice = 0;
+
     this.loadProducts();
   }
 
@@ -71,5 +87,14 @@ export class ListaProductosComponent implements OnInit {
         );
       });
     }
+  }
+
+  resetFilters() {
+    // Restablecer filtros de precio y búsqueda
+    this.minPrice = 0;
+    this.maxPrice = 0;
+    this.filterValue = '';
+    this.errorMessage = ''; // Limpiar mensaje de error
+    this.loadProducts(); // Recargar productos con filtros reiniciados
   }
 }
